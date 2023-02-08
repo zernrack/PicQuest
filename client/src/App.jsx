6c3854bca53ed759991f650d7675f3eb823d45ef
Renz <rackhold@gmail.com>
2023-02-09 02:52:01 +0800

    Add two inputs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,10 @@ import Registration from '../src/pages/registration/registration'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
     <Registration />
     </>
   )
 }
-
-export default App
--- client/src/pages/registration/registration.jsx
+++ client/src/pages/registration/registration.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import abstractArt from '../../assets/abstractArt.svg'
-
+import React from "react";
+import abstractArt from "../../assets/abstractArt.svg";
 
 export default function Registration() {
   return (
-    <>
-    <div className="container flex items-center">
-      <img src={abstractArt} alt="logo" />
-      <div className="container flex">
-        <h1 className="">Hello world</h1>
+    <body className="flex min-h-screen">
+      <div class="flex-1 bg-indigo-50">
+        <div className="logo-container container flex h-full max-lg:hidden">
+          <img
+            className="h-full w-auto object-cover"
+            src={abstractArt}
+            alt="logo"
+          />
+        </div>
       </div>
-    </div>
-    </>
-  )
+      <div class="flex-1 bg-indigo-50">
+        <h1 className=" text-7xl p-2 ">Let's Create Your Account</h1>
+
+        <div className="pt-80 sm:pr-0">
+          <input
+            className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
+            type="email"
+            placeholder="johndoe@gmail.com"
+            required
+          />
+        </div>
+        <div className="pt-3 sm:items-center">
+          <input
+            className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
+            type="email"
+            placeholder="johndoe@gmail.com"
+            required
+          />
+        </div>
+      </div>
+    </body>
+  );
 }
