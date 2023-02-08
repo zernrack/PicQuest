4b7d1c59ac24c57b2bff7eda8d0633ac69bdebc2
Renz <rackhold@gmail.com>
2023-02-08 17:34:12 +0800

    Edit registration page

--- client/src/pages/registration/registration.jsx
+++ client/src/pages/registration/registration.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import abstractArt from '../../assets/abstractArt.svg'
 
-export default function registration() {
+
+export default function Registration() {
   return (
-    <div>registration</div>
+    <>
+    <div className="container flex items-center">
+      <img src={abstractArt} alt="logo" />
+      <div className="container flex">
+        <h1 className="">Hello world</h1>
+      </div>
+    </div>
+    </>
   )
 }
