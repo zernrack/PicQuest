86c5378b5f792aff76608e3e2d344f8f3422a17b
Renz <rackhold@gmail.com>
2023-02-09 12:02:37 +0800

    Add changes to registration page

--- client/src/pages/registration/registration.jsx
+++ client/src/pages/registration/registration.jsx
@@ -1,36 +1,66 @@
 import React from "react";
-import abstractArt from "../../assets/abstractArt.svg";
+import AbstractArt from "../../assets/abstractArt.svg";
 
 export default function Registration() {
   return (
     <body className="flex min-h-screen">
       <div class="flex-1 bg-indigo-50">
-        <div className="logo-container container flex h-full max-lg:hidden">
+        <div className="logo-container container flex h-full md:hidden">
           <img
             className="h-full w-auto object-cover"
-            src={abstractArt}
+            src={AbstractArt}
             alt="logo"
           />
         </div>
       </div>
       <div class="flex-1 bg-indigo-50">
-        <h1 className=" text-7xl p-2 ">Let's Create Your Account</h1>
-
-        <div className="pt-80 sm:pr-0">
-          <input
-            className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
-            type="email"
-            placeholder="johndoe@gmail.com"
-            required
-          />
-        </div>
-        <div className="pt-3 sm:items-center">
-          <input
-            className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
-            type="email"
-            placeholder="johndoe@gmail.com"
-            required
-          />
+        <h1 className=" text-5xl font-bold pt-32 sm:text-5xl sm:-ml-52 -ml-18">
+          Create Account
+        </h1>
+        <h2 className="text-md sm:-ml-52 font-light pl-4 ">
+          Let's Create Your Account!
+        </h2>
+        <div className=" sm:-ml-44 ml-8 lg:-ml-18">
+          <div className="pt-32 sm:pt-32">
+            <input
+              className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
+              type="text"
+              placeholder="NAME"
+              required
+            />
+          </div>
+          <div className="pt-3 ">
+            <input
+              className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
+              type="email"
+              placeholder="EMAIL"
+              required
+            />
+          </div>
+          <div className="pt-3 ">
+            <input
+              className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
+              type="password"
+              placeholder="PASSWORD"
+              required
+            />
+          </div>
+          <div className="pt-3 ">
+            <input
+              className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
+              type="password"
+              placeholder="CONFIRM PASSWORD"
+              required
+            />
+            <div className="pt-10 pl-28">
+              <button
+                type="submit"
+                class=" text-center py-3 rounded bg-green-700 text-white hover:bg-green-dark hover:bg-green-400 w-44 h-12 pl-4"
+              >
+                Create Account
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </body>
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -6,10 +6,20 @@ module.exports = {
   ],
   theme: {
     screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
     },
     extend: {},
   },
