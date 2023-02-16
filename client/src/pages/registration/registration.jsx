0d2b7852c45fb115ed0d9a821421865bc3489b0c
Renz <rackhold@gmail.com>
2023-02-17 06:20:27 +0800

    Edit login and registration page

--- client/src/pages/registration/registration.jsx
+++ client/src/pages/registration/registration.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import AbstractArt from "../../assets/abstractArt.svg";
 
 export default function Registration() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleRegistration = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+    window.location.href = "/";
+  };
+
   return (
     <body className="flex min-h-screen">
       <div class="flex-1 bg-indigo-100">
@@ -20,13 +38,15 @@ export default function Registration() {
         <h2 className="text-md sm:-ml-52 font-light pl-4 pt-2 ">
           Let's Create Your Account!
         </h2>
-        <form action="/login">
+        <form onSubmit={handleRegistration}>
           <div className=" sm:-ml-44 ml-8 lg:-ml-18">
             <div className="pt-32 sm:pt-32">
               <input
                 className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
                 type="text"
                 placeholder="NAME"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
                 required
               />
             </div>
@@ -35,6 +55,8 @@ export default function Registration() {
                 className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
                 type="email"
                 placeholder="EMAIL"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
                 required
               />
             </div>
@@ -43,6 +65,8 @@ export default function Registration() {
                 className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
                 type="password"
                 placeholder="PASSWORD"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
                 required
               />
             </div>
@@ -51,18 +75,21 @@ export default function Registration() {
                 className="rounded border text-xl border-b-gray-300 w-96 h-12 pl-4"
                 type="password"
                 placeholder="CONFIRM PASSWORD"
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassword(event.target.value)}
                 required
               />
               <p class="text-sm font-thin mt-2 pt-1 mb-0">
                 Already Have Account?
                 <a
-                  href="/login"
+                  href="/"
                   class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
                 >
                   Login
                 </a>
               </p>
               <div className="pt-10 pl-28">
+                {error && <p className="text-red-600">{error}</p>}
                 <button
                   type="submit"
                   class=" text-center py-3 rounded bg-green-700 text-white hover:bg-green-dark hover:bg-green-400 w-44 h-12 pl-4 pr-4"
