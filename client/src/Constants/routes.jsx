67eca137f5af614513c518b1317a6a65134064af
Renz <rackhold@gmail.com>
2023-02-17 03:01:13 +0800

    Fix some syntax and cards

---
+++ client/src/Constants/routes.jsx
@@ -0,0 +1,8 @@
+const routes = {
+    home: "/home",
+    login: "/login",
+    register: "/register",
+
+};
+
+export default routes;
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import TabSelector from "./selector";
+import Logo from "../../assets/Logo.svg";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-row ml-28 mt-40  max-w-full gap-2">
+      <div className="flex justify-center items-center">
+        <img className="w-72 h-full" src={Logo} alt="" />
+      </div>
+      <div className="flex md:ml-9 flex-row ml-28 mt-10  max-w-full gap-2">
         <TabSelector />
       </div>
     </>
