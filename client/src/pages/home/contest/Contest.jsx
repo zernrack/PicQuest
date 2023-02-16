8c900d6617b11f8a0c3d77c570b0f9364a43b9ab
Renz <rackhold@gmail.com>
2023-02-17 04:14:24 +0800

    Fix home page and add winners

--- client/src/pages/home/contest/Contest.jsx
+++ client/src/pages/home/contest/Contest.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import ContestCard from "./ContestCard/Card";
 
 export default function Contest() {
+  function showAlert() {
+    alert("Thanks For Voting!");
+  }
   return (
     <>
+      <h1 className="text-5xl mb-10 font-sans font-black">VOTE NOW</h1>
       <div className="flex flex-wrap">
         <div class="p-4 bg-gray-200">
           <div class="rounded-lg shadow-lg bg-white max-w-sm">
@@ -13,12 +16,8 @@ export default function Contest() {
               alt=""
             />
             <div class="p-6">
-              <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-              <p class="text-gray-700 text-base mb-4">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
               <button
+                onClick={showAlert}
                 type="button"
                 class="relative left-28 md:left-20 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
@@ -32,16 +31,12 @@ export default function Contest() {
           <div class="rounded-lg shadow-lg bg-white max-w-sm">
             <img
               class="rounded-t-lg"
-              src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+              src="https://mdbootstrap.com/img/new/standard/nature/190.jpg"
               alt=""
             />
             <div class="p-6">
-              <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-              <p class="text-gray-700 text-base mb-4">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
               <button
+                onClick={showAlert}
                 type="button"
                 class="relative left-28 md:left-20 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
@@ -55,16 +50,12 @@ export default function Contest() {
           <div class="rounded-lg shadow-lg bg-white max-w-sm">
             <img
               class="rounded-t-lg"
-              src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+              src="https://mdbootstrap.com/img/new/standard/nature/189.jpg"
               alt=""
             />
             <div class="p-6">
-              <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-              <p class="text-gray-700 text-base mb-4">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
               <button
+                onClick={showAlert}
                 type="button"
                 class="relative left-28 md:left-20 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
@@ -78,16 +69,88 @@ export default function Contest() {
           <div class="rounded-lg shadow-lg bg-white max-w-sm">
             <img
               class="rounded-t-lg"
-              src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+              src="https://mdbootstrap.com/img/new/standard/nature/154.jpg"
               alt=""
             />
             <div class="p-6">
-              <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-              <p class="text-gray-700 text-base mb-4">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
               <button
+                onClick={showAlert}
+                type="button"
+                class="relative left-28 md:left-20 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                VOTE
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 bg-gray-200">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <img
+              class="rounded-t-lg"
+              src="https://mdbootstrap.com/img/new/standard/nature/114.jpg"
+              alt=""
+            />
+            <div class="p-6">
+              <button
+                onClick={showAlert}
+                type="button"
+                class="relative left-28 md:left-20 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                VOTE
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 bg-gray-200">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <img
+              class="rounded-t-lg"
+              src="https://mdbootstrap.com/img/new/standard/nature/124.jpg"
+              alt=""
+            />
+            <div class="p-6">
+              <button
+                onClick={showAlert}
+                type="button"
+                class="relative left-28 md:left-20 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                VOTE
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 bg-gray-200">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <img
+              class="rounded-t-lg"
+              src="https://mdbootstrap.com/img/new/standard/nature/153.jpg"
+              alt=""
+            />
+            <div class="p-6">
+              <button
+                onClick={showAlert}
+                type="button"
+                class="relative left-28 md:left-20 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                VOTE
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 bg-gray-200">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <img
+              class="rounded-t-lg"
+              src="https://mdbootstrap.com/img/new/standard/nature/140.jpg"
+              alt=""
+            />
+            <div class="p-6">
+              <button
+                onClick={showAlert}
                 type="button"
                 class="relative left-28 md:left-20 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
@@ -97,6 +160,7 @@ export default function Contest() {
           </div>
         </div>
       </div>
+      <div></div>
     </>
   );
 }
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import TabSelector from "./selector";
 import Logo from "../../assets/Logo.svg";
+import Login from "../login/login";
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
         <img className="w-72 h-full" src={Logo} alt="" />
+        <h1 className=" md:text-2xl font-serif text-3xl font-medium">
+          A place to show your photography skills
+        </h1>
       </div>
-      <div className="flex md:ml-9 flex-row ml-28 mt-10  max-w-full gap-2">
+      <div className="flex md:ml-9 flex-row items-start justify-start ml-28 mt-10  max-w-full gap-2">
         <TabSelector />
       </div>
     </>
